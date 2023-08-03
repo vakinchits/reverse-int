98f4ae2a279fa89982de21e1ba634185f1faa737
vakinchits <vakinchits@gmail.com>
2023-08-03 09:12:00 +0200

    feat: Added main function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN = n.toString();
+    let arrN = Array.from(strN);
+    let revA = arrN.reverse();
+    let revR = revA.join("");
+    if (revA[revA.length - 1] == "-") {
+        let revS = revR.substring(0, revR.length - 1);
+        return Number(revS);
+    } else {
+        let revS = revR.substring(0, revR.length);
+        return Number(revS);
+    }
+};
